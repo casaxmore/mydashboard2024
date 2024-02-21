6b012cdbd4b346f9077e444ce8bf9b14b1ea5e0b
casaxmore <casaxmore@gmail.com>
2024-02-21 10:20:16 +0100

    primera subida

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    RouterModule,
+    SidemenuComponent
+  ],
+  templateUrl: './dashboard.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class DashboardComponent { }
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidemenuComponent {
+
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter( route => route && route.path)
+    .filter( route => !route.path?.includes(':'))
+    .filter( route => !route.path?.includes('**'));
+
+  constructor() {
+   /*  const dashboardRoutes = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter( route => route && route.path)
+    .filter( route => !route.path?.includes(':'))
+    .filter( route => !route.path?.includes('**'))
+    console.log(dashboardRoutes); */
+  }
+
+
+}
